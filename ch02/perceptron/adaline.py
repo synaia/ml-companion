2651b3f8fe7500885ea5816d4fa4147c15243f4d
beltrewilton <beltre.wilton@gmail.com>
2022-11-05 17:00:43 -0400

    Adeline SGD impl basis

--- ch02/perceptron/adaline.py
+++ ch02/perceptron/adaline.py
@@ -10,6 +10,8 @@ class AdalineGD:
         learning rate (btw 0. and 1.)
     n_iter: int
         passes over the training dataset.
+    shuffle: bool (default : True)
+        shuffles training data every epoch if True.
     random_state: int
         random number generator for random weight initialization.
 
@@ -23,9 +25,11 @@ class AdalineGD:
         mean squared error loss function values in each epoch.
 
     """
-    def __init__(self, eta=0.01, n_iter=50, randon_state=42):
+    def __init__(self, eta=0.01, n_iter=50, shuffle=True, randon_state=42):
         self.eta = eta
         self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
         self.random_state = randon_state
 
     def fit(self, X, y):
@@ -40,23 +44,70 @@ class AdalineGD:
         -------
         self: object
         """
-        rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
-        self.b_ = np.float_(0.0)
+        self._initialize_weights(X.shape[1])
         self.losses_ = []
 
         for i in range(self.n_iter):
-            net_input = self.net_input(X) # linear relat.
-            output = self.activation(net_input)
-            errors = (y - output) # knows as 'residual'
-            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
-            self.b_ += self.eta * 2.0 * errors.mean()
-            loss = (errors**2).mean()
-            self.losses_.append(loss)
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            losses = []
+            for xi, target in zip(X, y):
+                losses.append(self._update_weights(xi, target))
+            avg_loss = np.mean(losses)
+            self.losses_.append(avg_loss)
         return self
 
+    def partial_fit(self, X, y):
+        """Fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        """Shuffle training data"""
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        """Initialize weights to small random mnumers"""
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=m)
+        self.b_ = np.float_(0.)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        """Apply Adeline learning rule to update the weights"""
+        output = self.activation(self.net_input(xi))
+        error = (target - output)   # residuals
+        self.w_ += self.eta * 2.0 * xi * (error)
+        self.b_ += self.eta * 2.0 * error
+        loss = error**2
+        return loss
+
+
+        #     net_input = self.net_input(X) # linear relat.
+        #     output = self.activation(net_input)
+        #     errors = (y - output) # knows as 'residual'
+        #     self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+        #     self.b_ += self.eta * 2.0 * errors.mean()
+        #     loss = (errors**2).mean()
+        #     self.losses_.append(loss)
+        # return self
+    #
+    # def _initialize_weights(self, m):
+    #     """Initialize weights to small random numbers"""
+    #     self.rgen = np.random.RandomState(self.random_state)
+    #     self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=m)
+    #     self.b_ = np.float_(0.0)
+    #     self.w_initialized = True
+
     def net_input(self, X):
-        """Calculate the net input"""
+        """Calculate the net input (dot product)"""
         return np.dot(X, self.w_) + self.b_
 
     def activation(self, X):
--- ch02/perceptron/adeline_main.py
+++ ch02/perceptron/adeline_main.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 from adaline import AdalineGD
+from helper import plot_decision_regions
 
 # load iris data
 s = '../../dataset/iris.data'
@@ -15,18 +16,34 @@ y = np.where(y == 'Iris-setosa', 0, 1)
 # extract sepal lenght and pedal length
 X = df.iloc[0:100, [0, 2]].values
 
+# standarize X's
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+
+ada1 = AdalineGD(n_iter=50, eta=0.01, randon_state=1).fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada1)
+plt.title('Adaline - Stochastic Gradient descent')
+plt.xlabel('Sepal length [standarized]')
+plt.ylabel('Petal length [standarized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
-ada1 = AdalineGD(n_iter=15, eta=0.1).fit(X, y)
-ax[0].plot(range(1, len(ada1.losses_) +1), np.log10(ada1.losses_), marker='o' )
+ax[0].plot(range(1, len(ada1.losses_) +1), ada1.losses_, marker='o' )
 ax[0].set_xlabel('Epochs')
-ax[0].set_ylabel('log(Mean squared error)')
-ax[0].set_title('Adaline - Learning rate 0.1')
+ax[0].set_ylabel('Average loss')
+ax[0].set_title('Adaline - Learning rate 0.01')
 
-ada2 = AdalineGD(n_iter=15, eta=0.0001).fit(X, y)
-ax[1].plot(range(1, len(ada2.losses_) +1), np.log10(ada2.losses_), marker='o' )
+ada2 = AdalineGD(n_iter=50, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.losses_) +1), ada2.losses_, marker='o' )
 ax[1].set_xlabel('Epochs')
-ax[1].set_ylabel('log(Mean squared error)')
+ax[1].set_ylabel('Average loss')
 ax[1].set_title('Adaline - Learning rate 0.0001')
+
 
 plt.show()
