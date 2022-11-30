13e762059676e2627cea33d0512ba4982976768d
beltrewilton <beltre.wilton@gmail.com>
2022-11-30 15:04:22 -0400

    playing with majority vote, its awesome!

--- ch07/mvc-impl.py
+++ ch07/mvc-impl.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -9,6 +10,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from majorityvoteclassifier import MajorityVoteClassifier
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
 
 
 iris = datasets.load_iris()
@@ -19,7 +22,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 
 clf1 = LogisticRegression(penalty='l2', C=0.001, solver='lbfgs', random_state=1)
 clf2 = DecisionTreeClassifier(max_depth=1, criterion='entropy', random_state=0)
-clf3 = KNeighborsClassifier(n_neighbors=1, p=2,  metric='minkowski')
+clf3 = KNeighborsClassifier(n_neighbors=1, p=2,  metric='minkowski') # p:2 => euclidean_distance
 
 pipe1 = Pipeline([['sc', StandardScaler()], ['clf', clf1]])
 pipe3 = Pipeline([['sc', StandardScaler()], ['clf', clf3]])
@@ -29,6 +32,7 @@ print('10-fold cross validation:\n')
 for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
     scores = cross_val_score(estimator=clf, X=X_train, y=y_train, cv=10, scoring='roc_auc')
     print(f'ROC AUC: {scores.mean():.2f} (+/- {scores.std():.2f}) [{label}]')
+
 print('-'*100)
 
 mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
@@ -37,3 +41,21 @@ all_clf = [pipe1, clf2, pipe3, mv_clf]
 for clf, label in zip(all_clf, clf_labels):
     scores = cross_val_score(estimator=clf, X=X_train, y=y_train, cv=10, scoring='roc_auc')
     print(f'ROC AUC: {scores.mean():.2f} (+/- {scores.std():.2f}) [{label}]')
+
+colors = ['black', 'orange', 'blue', 'green']
+linestyles = [':', '--', '-.', '-']
+for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
+    y_pred = clf.fit(X_train, y_train).predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+    plt.plot(fpr, tpr, color=clr, linestyle=ls, label=f'{label} (auc = {roc_auc:0.2f}) | thresholds: {np.mean(thresholds):.2f}')
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1], linestyle='--', color='gray', linewidth=2)
+plt.xlim([-0.1, 1.1])
+plt.ylim([-0.1, 1.1])
+plt.grid(alpha=0.2)
+plt.xlabel('False positive rate (FPR)')
+plt.ylabel('True positive rate (TPR)')
+plt.show()
+
+
