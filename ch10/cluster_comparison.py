7e95c9e8c8472bc247b71cf95c10f38a61d1e478
beltrewilton <beltre.wilton@gmail.com>
2022-12-31 11:43:32 -0400

    kmean vs agglomerative vs dbscan

---
+++ ch10/cluster_comparison.py
@@ -0,0 +1,41 @@
+from sklearn.datasets import make_moons
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+
+random_state = 0
+
+X, y = make_moons(n_samples=500, noise=0.05, random_state=random_state)
+
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 3))
+km = KMeans(n_clusters=2, random_state=random_state)
+y_km = km.fit_predict(X)
+ax1.scatter(X[y_km == 0, 0], X[y_km == 0, 1], c='lightblue', edgecolor='black', marker='o', s=40, label='cluster 1')
+ax1.scatter(X[y_km == 1, 0], X[y_km == 1, 1], c='red', edgecolor='black', marker='s', s=40, label='cluster 2')
+ax1.set_title('K-means clustering')
+ax1.set_xlabel('Feature 1')
+ax1.set_ylabel('Feature 2')
+
+ac = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='complete')
+y_ac = ac.fit_predict(X)
+ax2.scatter(X[y_ac == 0, 0], X[y_ac == 0, 1], c='lightblue', edgecolor='black', marker='o', s=40, label='cluster 1')
+ax2.scatter(X[y_ac == 1, 0], X[y_ac == 1, 1], c='red', edgecolor='black', marker='s', s=40, label='cluster 2')
+ax2.set_title('Agglomerative clustering')
+ax2.set_xlabel('Feature 1')
+ax2.set_ylabel('Feature 2')
+
+db = DBSCAN(eps=0.2, min_samples=5, metric='euclidean')
+y_db = db.fit_predict(X)
+ax3.scatter(X[y_db == 0, 0], X[y_db == 0, 1], c='lightblue', edgecolor='black', marker='o', s=40, label='cluster 1')
+ax3.scatter(X[y_db == 1, 0], X[y_db == 1, 1], c='red', edgecolor='black', marker='s', s=40, label='cluster 2')
+ax3.set_title('DBSCAN clustering')
+ax3.set_xlabel('Feature 1')
+ax3.set_ylabel('Feature 2')
+
+
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
